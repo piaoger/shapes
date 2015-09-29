e8f1956e716f161e4a7a53d4c360ff214a29a941
piaoger <piaoger@yahoo.com>
2015-09-29 17:38:14 +0800

    let it work

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ use router::Router;
 use serde::json;
 use std::io::Read;
 use std::sync::{Arc, Mutex};
-//use std::fs::File;
 use std::path::Path;
 
 // static files
@@ -30,6 +29,7 @@ use std::thread;
 
 use websocket::header::WebSocketProtocol;
 
+extern crate rand;
 
 #[derive(Serialize, Deserialize)]
 struct Greeting {
@@ -38,12 +38,15 @@ struct Greeting {
 
 
 fn setup_httpserv() {
+
+    // Start listening for http connections
+
     let greeting = Arc::new(Mutex::new(Greeting { msg: "Hello, World".to_string() }));
     let greeting_clone = greeting.clone();
     
     let mut router = Router::new();
 
-    router.get("/", move |r: &mut Request| hello_world(r, &greeting.lock().unwrap()));
+    router.get("/", move |r: &mut Request | hello_world(r, &greeting.lock().unwrap()));
     router.post("/set", move |r: &mut Request| set_greeting(r, &mut greeting_clone.lock().unwrap()));
     
     fn hello_world(_: &mut Request, greeting: &Greeting) -> IronResult<Response> {
@@ -65,12 +68,8 @@ fn setup_httpserv() {
     mount.mount("/api", router);
     mount.mount("/", Static::new(Path::new("www")));
 
-        // Listen
+    // Listen
     Iron::new(mount).http("127.0.0.1:8080").unwrap();
-
-    //
-
-
 }
 
 fn setup_wsserv() {
@@ -78,6 +77,7 @@ fn setup_wsserv() {
     let server = Server::bind("127.0.0.1:8021").unwrap();
 
     for connection in server {
+
         // Spawn a new thread for each connection.
         thread::spawn(move || {
 
@@ -88,19 +88,19 @@ fn setup_wsserv() {
             //   html: var socket = new WebSocket("ws://localhost:8080", "rust-websocket");
             // If no websocket protocol check here, there will have error in Chromimum browsers: chrome, opera....
             // Let's also check the protocol - if it's not what we want, then fail the connection
-            // if request.protocol().is_none() || !request.protocol().unwrap().contains(&"rust-websocket".to_string()) {
-            //     let response = request.fail();
-            //     let _ = response.send_into_inner();
-            //     return;
-            // }
+            if request.protocol().is_none() || !request.protocol().unwrap().contains(&"rust-websocket".to_string()) {
+                let response = request.fail();
+                let _ = response.send_into_inner();
+                return;
+            }
 
-            // let mut response = request.accept(); // Generate a response
-            // response.headers.set(WebSocketProtocol(vec!["rust-websocket".to_string()])); // Send a Sec-WebSocket-Protocol header
+            let mut response = request.accept(); // Generate a response
+            response.headers.set(WebSocketProtocol(vec!["rust-websocket".to_string()])); // Send a Sec-WebSocket-Protocol header
+            let mut client = response.send().unwrap(); // Send the response
+
+            // let response = request.accept(); // Form a response
             // let mut client = response.send().unwrap(); // Send the response
 
-            let response = request.accept(); // Form a response
-            let mut client = response.send().unwrap(); // Send the response
-            
             let ip = client.get_mut_sender()
                 .get_mut()
                 .peer_addr()
@@ -112,21 +112,43 @@ fn setup_wsserv() {
             client.send_message(message).unwrap();
             
             let (mut sender, mut receiver) = client.split();
-            
+
             for message in receiver.incoming_messages() {
                 let message = message.unwrap();
                 
                 match message {
+
+                    // close
                     Message::Close(_) => {
                         let message = Message::Close(None);
                         sender.send_message(message).unwrap();
-                        println!("Client {} disconnected", ip);
                         return;
                     }
+
+                    // Ping/Pong Frame
+                    // Javascript does not implement ping/pong yet.
                     Message::Ping(data) => {
                         let message = Message::Pong(data);
                         sender.send_message(message).unwrap();
                     }
+
+                    //  JavaScript WebSocket binary type: blob, databuffer
+                    //  ws.binaryType = blob or arraybuffer
+                    //  arraybuffer is default binary type.
+                    Message::Binary(data) => {
+                        println!("{:?}", data);
+                       let  msg:Vec<u8>  = vec![3,4,5,6,7];
+                        sender.send_message(Message::Binary(msg)).unwrap();
+                    }
+
+                    // Text Frame
+                    Message::Text(data) => {
+                        let ru8: u8 = rand::random::<u8>() % 100;
+                        let message = Message::Text(format!("randon number: {} for {}", ru8, data).to_string());
+
+                        sender.send_message(message).unwrap();
+                    }
+        
                     _ => sender.send_message(message).unwrap(),
                 }
             }
@@ -136,29 +158,15 @@ fn setup_wsserv() {
 
 
  
-
-use std::net::Ipv4Addr;
-
-#[derive(Debug)]
-enum HostAddress {
-    DomainName(String),
-    Ip(Ipv4Addr),
-}
-
-#[derive(Debug)]
-struct Port(u32);
-
-#[derive(Debug)]
-struct ConnectionLimit(u32);
-
  
 
 fn main() {
-    // Start listening for http connections
+
     thread::spawn(move || {
-        setup_httpserv() ;
+      setup_httpserv() ;
+
     });
-    
+
     setup_wsserv();
-    //mainanymap();
+    
 }
